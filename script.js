6d1370c8740b2eb0d16e26331c1ea7adb00f1538
Oleksandr Topolnytskyi <104443619+Topalex1971@users.noreply.github.com>
2022-06-26 17:19:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function openbox1(box1) {
+    display = document.getElementById('box1').style.display;
+        if (display == "none"){
+            document.getElementById('box1').style.display = "block";
+        }
+        else {
+            document.getElementById('box1').style.display = "none";
+        }
+    }
+
+function openbox2(box2) {
+        display = document.getElementById('box2').style.display;
+            if (display == "none"){
+                document.getElementById('box2').style.display = "block";
+            }
+            else {
+                document.getElementById('box2').style.display = "none";
+            }
+        }
+
+function openbox3(box3) {
+        display = document.getElementById('box3').style.display;
+            if (display == "none"){
+                    document.getElementById('box3').style.display = "block";
+            }
+            else {
+                    document.getElementById('box3').style.display = "none";
+            }
+    }       
